8d8d7ebd2def0e7f0b0e071e8fdc7a662e57a965
hyeyun <sally288@naver.com>
2018-06-02 04:55:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sally/seein/Config.java
@@ -0,0 +1,13 @@
+package com.example.sally.seein;
+
+/**
+ * Created by sally on 2018-06-01.
+ */
+
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://35.231.181.115:80/AndroidFileUpload/FileUpload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "http://35.231.181.115:80/AndroidFileUpload/uploads";
+}
